aa1370b58610611c5cbd0dff80de7573d7ca79e9
Irish Micole Cando <irishmicolecando@gmail.com>
2022-12-15 22:06:01 +0800

    Declare needed variables and comment conversion units

---
+++ script.js
@@ -0,0 +1,15 @@
+'use strict'
+
+const inputtedUnit = document.getElementById("number-to-be-converted");
+const convertBtn = document.getElementById("convert-btn");
+const lengthContainer = document.getElementById("length");
+const volumeContainer = document.getElementById("volume");
+const massContainer = document.getElementById("mass");
+
+
+/*
+conversion units
+1 meter = 3.281 feet
+1 liter = 0.264 gallon
+1 kilogram = 2.204 pound
+*/
